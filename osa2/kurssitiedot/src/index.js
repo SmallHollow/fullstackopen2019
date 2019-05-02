3f98738316900a1162be5633d300bcd730f115a6
Esa Onttonen <esa@onttonen.info>
2019-05-02 19:45:54 +0300

    2.4 kurssitiedot step9

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -26,50 +26,72 @@ const Total = ({parts}) => {
   )
 }
 
-const Header = ({text}) => {
-  return <h1>{text}</h1>
+const CourseHeader = ({text}) => {
+  return <h2>{text}</h2>
 }
 
 const Course = ({course}) => {
   return (
     <div>
-      <Header text={course.name} />
+      <CourseHeader text={course.name} />
       <Content parts={course.parts} />
+      <Total parts={course.parts}/>
     </div>
   )
 }
 
+const Courses = ({courses}) => {
+  const courselist = () => courses.map(course => <Course course={course} key={course.id} />)
+  return (
+    <div>{courselist()}</div>
+  )
+}
+
 const App = () => {
-  const course = {
-    name: 'Half Stack -sovelluskehitys',
-    parts: [
+  const courses = [
       {
-        name: 'Reactin perusteet',
-        exercises: 10,
-        id: 1
+        name: 'Half Stack -sovelluskehitys',
+        id: 1,
+        parts: [
+          {
+            name: 'Reactin perusteet',
+            exercises: 10,
+            id: 1
+          },
+          {
+            name: 'Tiedonvälitys propseilla',
+            exercises: 7,
+            id: 2
+          },
+          {
+            name: 'Komponenttien tila',
+            exercises: 14,
+            id: 3
+          }
+        ]
       },
       {
-        name: 'Tiedonvälitys propseilla',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'Komponenttien tila',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 7,
-        id: 4
+        name: 'Node.js',
+        id: 2,
+        parts: [
+          {
+            name: 'Routing',
+            exercises: 3,
+            id: 1
+          },
+          {
+            name: 'Middlewaret',
+            exercises: 7,
+            id: 2
+          }
+        ]
       }
     ]
-  }
 
   return (
     <div>
-      <Course course={course} />
-      <Total parts={course.parts}/>
+      <h1>Opetusohjelma</h1>
+      <Courses courses={courses} />
     </div>
   )
 }
