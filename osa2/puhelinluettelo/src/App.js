55494848c53a0ba299037ced6c0167c66dedf0fc
Esa Onttonen <esa@onttonen.info>
2019-05-06 10:19:53 +0300

    2.8 puhelinluettelo step3

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,21 +1,23 @@
 import React, { useState } from 'react'
 
-const Name = ({name}) => {
+const Name = ({name, number}) => {
   return (
-    <li>{name}</li>
+    <li>{name} {number}</li>
   )
 }
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '' }
   ])
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
     const nameObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     let exists = false
     persons.forEach(person => {
@@ -29,21 +31,25 @@ const App = () => {
       setPersons(persons.concat(nameObject))
     }
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
 
-  const listNames = () => persons.map(person => <Name key={person.name} name={person.name} />)
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const listNames = () => persons.map(person => <Name key={person.name} name={person.name} number={person.number} />)
 
   return (
     <div>
       <h2>Puhelinluettelo</h2>
       <form onSubmit={addName}>
-        <div>
-          nimi: <input value={newName} onChange={handleNameChange} />
-        </div>
+        <div>nimi: <input value={newName} onChange={handleNameChange} /></div>
+        <div>numero: <input value={newNumber} onChange={handleNumberChange} /></div>
         <div>
           <button type="submit">lisää</button>
         </div>
