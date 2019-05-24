5d0f2f6a6ba9de0945d6fad44083c0a79abb3f1c
Esa Onttonen <esa@onttonen.info>
2019-05-24 09:46:53 +0300

    4.2 blogilista, step2

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -2,6 +2,9 @@ const config = require('./utils/config')
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const blogsRouter = require('./controllers/blogs')
+app.use('/api/blogs', blogsRouter)
+
 const cors = require('cors')
 const mongoose = require('mongoose')
 
@@ -13,22 +16,5 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true })
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 module.exports = app
