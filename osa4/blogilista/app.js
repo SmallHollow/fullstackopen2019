5b6a384bcac19481150cf07476d8d1b3874e039c
Esa Onttonen <esa@onttonen.info>
2019-05-24 09:41:14 +0300

    4.2 blogilista, step2 - erillinen app.js

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,34 @@
+const config = require('./utils/config')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const Blog = require('./models/blog')
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl, { useNewUrlParser: true })
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+module.exports = app
--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,37 +1,9 @@
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const mongoose = require('mongoose')
 const config = require('./utils/config')
 
-const Blog = require('./models/blog')
+const server = http.createServer(app)
 
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true })
-
-app.use(cors())
-app.use(bodyParser.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-app.listen(config.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`)
+server.listen(config.PORT, () => {
+  console.log(`Palvelin osoitteessa ${config.PORT}`)
 })
