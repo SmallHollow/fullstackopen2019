f6109b1c6163af7075e11addc9d8e61d7c85e1be
Esa Onttonen <esa@onttonen.info>
2019-05-25 10:52:53 +0300

    4.4: apufunktioita ja yksikkötestejä, step2

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -2,6 +2,20 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 0
+  } else if (blogs.length === 1) {
+    return blogs[0].likes
+  } else {
+    likeCount = 0
+    blogs.forEach(blog => {
+      likeCount += blog.likes
+    })
+    return likeCount
+  }
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
