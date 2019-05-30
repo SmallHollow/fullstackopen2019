a8686d706578e4036dc30fe9a92ddaedeab351e4
Esa Onttonen <esa@onttonen.info>
2019-05-30 13:09:14 +0300

    Korjattu routerin järjestys.

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -3,15 +3,11 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const blogsRouter = require('./controllers/blogs')
-app.use('/api/blogs', blogsRouter)
-
-const cors = require('cors')
 const mongoose = require('mongoose')
 
-const mongoUrl = config.mongodb_uri
-mongoose.connect(mongoUrl, { useNewUrlParser: true })
+mongoose.connect(config.mongodb_uri, { useNewUrlParser: true })
 
-app.use(cors())
 app.use(bodyParser.json())
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
