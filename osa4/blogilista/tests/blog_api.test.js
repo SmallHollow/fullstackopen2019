1abb84e66abf3b80aa7c5a9bca1b4c5d9fa0180e
Esa Onttonen <esa@onttonen.info>
2019-05-30 13:49:13 +0300

    4.9-4.10: blogilistan testit, step2-3

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -14,7 +14,36 @@ test('blogi palauttaa jsonia', async () => {
 test('listassa on x blogia', async () => {
   const response = await api.get('/api/blogs')
 
-  expect(response.body.length).toBe(1)
+  expect(response.body.length).toBe(2)
+})
+
+test('id-kenttä', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body[0].id).toBeDefined()
+})
+
+test('uuden blogin lisäys', async () => {
+  let response = await api.get('/api/blogs')
+  const blogCount = response.body.length
+
+  const testBlog = {
+    title: 'Testausblogi',
+    author: 'Testeri',
+    url: 'http://www.yle.fi',
+    likes: 6
+  }
+
+  response = await api.post('/api/blogs').send(testBlog)
+
+  response = await api.get('/api/blogs')
+  expect(response.body.length).toBe(blogCount+1)
+  const lastBlog = response.body[blogCount]
+  expect(lastBlog.title).toBe(testBlog.title)
+  expect(lastBlog.author).toBe(testBlog.author)
+  expect(lastBlog.url).toBe(testBlog.url)
+  expect(lastBlog.likes).toBe(testBlog.likes)
+
 })
 
 afterAll(() => {
