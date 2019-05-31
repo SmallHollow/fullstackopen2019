5b0f4c324fdbcc69e17ee7eeb2fa66f76dbd4e37
Esa Onttonen <esa@onttonen.info>
2019-05-31 12:00:59 +0300

    korjaus 4.11:een.

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -15,7 +15,7 @@ blogsRouter.post('/', async (request, response) => {
         title: body.title,
         author: body.author,
         url: body.url,
-        likes: body.likes || 0
+        likes: body.likes === undefined ? 0 : body.likes
       })
       const savedBlog = await blog.save()
       response.json(savedBlog.toJSON())
