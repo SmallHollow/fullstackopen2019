725dc66571b0e9bb33324c26da1b653998ae15d6
Esa Onttonen <esa@onttonen.info>
2019-06-02 11:31:04 +0300

    4.14* blogilistan laajennus, step2 (ei testiä)

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -34,7 +34,23 @@ blogsRouter.delete('/:id', async (request, response) => {
   } catch (exception) {
     console.log("Poisto ei onnistunut. ")
   }
+})
 
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+
+  const blog = {
+    likes: body.likes
+  }
+
+  console.log(body.likes)
+
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true})
+    response.json(updatedBlog.toJSON())
+  } catch (exception) {
+    console.log("Päivitys ei onnistunut. ")
+  }
 })
 
 module.exports = blogsRouter
