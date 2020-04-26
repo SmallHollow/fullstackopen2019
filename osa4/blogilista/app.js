8f1f6f9127feb384c2965077e2507f06b7c5114c
Esa Onttonen <esa@onttonen.info>
2020-04-26 12:30:50 +0300

    Päivitystä v. 2020 kursisn toteutukseen.

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -1,13 +1,12 @@
 const config = require('./utils/config')
 const express = require('express')
-const bodyParser = require('body-parser')
 const app = express()
 const blogsRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
 
-mongoose.connect(config.mongodb_uri, { useNewUrlParser: true })
+mongoose.connect(config.mongodb_uri, { useNewUrlParser: true, useUnifiedTopology: true })
 
-app.use(bodyParser.json())
+app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 
 module.exports = app
