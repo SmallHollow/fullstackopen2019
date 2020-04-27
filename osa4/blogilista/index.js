a5a47388a66b56ceb4d9efb62034ba3756add8ef
Esa Onttonen <esa@onttonen.info>
2020-04-27 11:51:34 +0300

    Erillinen loggeri + vakiot ISOILLA KIRJAIMILLA.

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,9 +1,10 @@
 const app = require('./app')
 const http = require('http')
 const config = require('./utils/config')
+const logger = require('./utils/logger')
 
 const server = http.createServer(app)
 
-server.listen(config.port, () => {
-  console.log(`Palvelin osoitteessa ${config.port}`)
+server.listen(config.PORT, () => {
+  logger.info(`Palvelin osoitteessa ${config.PORT}`)
 })
--- osa4/blogilista/utils/config.js
+++ osa4/blogilista/utils/config.js
@@ -1,13 +1,13 @@
 require('dotenv').config()
 
-let port = process.env.PORT
-let mongodb_uri = process.env.MONGODB_URI
+let PORT = process.env.PORT
+let MONGODB_URI = process.env.MONGODB_URI
 
 if (process.env.NODE_ENV === 'test') {
-  mongodb_uri = process.env.TEST_MONGODB_URI
+  MONGODB_URI = process.env.TEST_MONGODB_URI
 }
 
 module.exports = {
-  mongodb_uri,
-  port
+  MONGODB_URI,
+  PORT
 }
