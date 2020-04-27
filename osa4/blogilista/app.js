82e57207b2664d0b5f0bc257ebe2f8707581757e
Esa Onttonen <esa@onttonen.info>
2020-04-27 11:52:15 +0300

    4.15: blogilistan laajennus, step4

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -2,11 +2,13 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const mongoose = require('mongoose')
 
-mongoose.connect(config.mongodb_uri, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
