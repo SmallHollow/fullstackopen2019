c4fe00849bb25523155c40d0c6fafd87a70c2449
Esa Onttonen <esa@onttonen.info>
2021-03-15 11:39:51 +0200

    2.5: erillinen moduuli

---
+++ osa2/kurssitiedot/src/Course.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const Header = ({ course }) => {
+  return <h2>{course}</h2>;
+};
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.name} name={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((acc, part) => {
+    return acc + part.exercises;
+  }, 0);
+  return (
+    <p>
+      <b>total of {total} exercises</b>
+    </p>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
+  );
+};
+
+export default Course;
--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -1,48 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Header = ({ course }) => {
-  return <h2>{course}</h2>;
-};
-
-const Part = ({ name, exercises }) => {
-  return (
-    <p>
-      {name} {exercises}
-    </p>
-  );
-};
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map((part) => (
-        <Part key={part.name} name={part.name} exercises={part.exercises} />
-      ))}
-    </div>
-  );
-};
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((acc, part) => {
-    return acc + part.exercises;
-  }, 0);
-  return (
-    <p>
-      <b>total of {total} exercises</b>
-    </p>
-  );
-};
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </>
-  );
-};
+import Course from './Course';
 
 const App = () => {
   const courses = [
