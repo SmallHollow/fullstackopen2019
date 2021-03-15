8dfea4fae299b6af1e1d41fdb59eb572b52655bb
Esa Onttonen <esa@onttonen.info>
2021-03-15 09:54:06 +0200

    Kopioitu osa1:n kurssitiedot pohjaksi osa2:een.

---
+++ osa2/kurssitiedot/src/index.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.kurssinimi}</h1>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.nimi} {props.maara}</p>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+  return (
+    <div>
+    <Part nimi={props.parts[0].name} maara={props.parts[0].exercises} />
+    <Part nimi={props.parts[1].name} maara={props.parts[1].exercises} />
+    <Part nimi={props.parts[2].name} maara={props.parts[2].exercises} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  const yht = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises
+  return (
+    <p>yhteensä {yht} tehtävää</p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack -sovelluskehitys',
+    parts: [
+      {
+        name: 'Reactin perusteet',
+        exercises: 10
+      },
+      {
+        name: 'Tiedonvälitys propseilla',
+        exercises: 7
+      },
+      {
+        name: 'Komponenttien tila',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header kurssinimi={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
