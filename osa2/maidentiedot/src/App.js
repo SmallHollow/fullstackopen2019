a38ac071446718b6f788844a844bb454af7af95d
Esa Onttonen <esa@onttonen.info>
2021-03-15 20:20:43 +0200

    2.13*: maiden tiedot, step2

--- osa2/maidentiedot/src/App.js
+++ osa2/maidentiedot/src/App.js
@@ -9,38 +9,53 @@ const Input = ({ country, handler }) => {
   );
 };
 
-const Countries = ({ countries, countryFilter }) => {
+const Country = ({ country }) => {
+  return (
+    <div>
+      <h1>{country.name}</h1>
+      <div>capital {country.capital}</div>
+      <div>population {country.population}</div>
+      <h2>Languages</h2>
+      <ul>
+        {country.languages.map((lang) => (
+          <li key={lang.name}>{lang.name}</li>
+        ))}
+      </ul>
+      <img src={country.flag} alt="Flag" width="200px" />
+    </div>
+  );
+};
+
+const Countries = ({ countries, countryFilter, handleShow }) => {
   const filteredCountries = countries.filter((country) =>
     country.name.toUpperCase().includes(countryFilter.toUpperCase())
   );
   if (filteredCountries.length > 10) {
     return <div>Too many matches, specify another filter</div>;
   }
+
   return filteredCountries.length === 1 ? (
+    <Country country={filteredCountries[0]} />
+  ) : (
     <div>
-      <h1>{filteredCountries[0].name}</h1>
-      <div>capital {filteredCountries[0].capital}</div>
-      <div>population {filteredCountries[0].population}</div>
-      <h2>Languages</h2>
       <ul>
-        {filteredCountries[0].languages.map((lang) => (
-          <li key={lang.name}>{lang.name}</li>
+        {filteredCountries.map((country) => (
+          <li key={country.name}>
+            {country.name}{' '}
+            <button value={country.name} onClick={handleShow}>
+              show
+            </button>
+          </li>
         ))}
       </ul>
-      <img src={filteredCountries[0].flag} alt="Flag" width="200px" />
     </div>
-  ) : (
-    <ul>
-      {filteredCountries.map((country) => (
-        <li key={country.name}>{country.name}</li>
-      ))}
-    </ul>
   );
 };
 
 const App = () => {
   const [country, setCountry] = useState('');
   const [countries, setCountries] = useState([]);
+  const [selected, setSelected] = useState('');
 
   useEffect(() => {
     axios.get('https://restcountries.eu/rest/v2/all').then((response) => {
@@ -50,12 +65,26 @@ const App = () => {
 
   const handleFilter = (event) => {
     setCountry(event.target.value);
+    setSelected('');
+  };
+
+  const handleShow = (event) => {
+    setSelected(event.target.value);
   };
 
   return (
     <div>
       <Input value={country} handler={handleFilter} />
-      <Countries countries={countries} countryFilter={country} />
+      {country !== '' && (
+        <Countries
+          countries={countries}
+          countryFilter={country}
+          handleShow={handleShow}
+        />
+      )}
+      {selected !== '' && (
+        <Country country={countries.find((c) => c.name === selected)} />
+      )}
     </div>
   );
 };
